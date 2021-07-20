ab44590a03ed444b8dcfa8eb06b91f0b6171d6f2
Greg Nemeth <GNemeth@qa.com>
2021-07-20 10:39:53 +0000

    "GN: added route and html for adding groups"

--- tipsy_tuesday/application/forms.py
+++ tipsy_tuesday/application/forms.py
@@ -5,3 +5,7 @@ class AddIngredient(FlaskForm):
     ingredient = StringField('Ingredient here')
     ing_group = SelectField('Choose ingredient group', choices=[])
     submit = SubmitField('Add ingredient')
+
+class AddGroup(FlaskForm):
+    group_name = StringField('Ingredient Group')
+    submit = SubmitField('Add Group')
--- tipsy_tuesday/application/routes.py
+++ tipsy_tuesday/application/routes.py
@@ -1,13 +1,16 @@
+from re import U
 from flask import redirect, request, url_for
 from flask.templating import render_template
 from application import app, db
 from application.models import Cocktailrecipes, Ingredientgroup, Ingredient, Quantity, Junction
-from application.forms import AddIngredient
+from application.forms import AddGroup, AddIngredient
 
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    groups = Ingredientgroup.query.all()
+
+    return render_template('home.html', groups=groups)
 
 
 @app.route('/add_ingredient', methods=["GET", "POST"])
@@ -28,6 +31,20 @@ def add_ingredient():
         for ing_group in ing_groups:
             form.ing_group.choices.append((ing_group.ing_group_id, ing_group.group_name))
         return render_template('add_ingredient.html', form=form)
+
+@app.route('/add_group', methods=["GET", "POST"])
+def add_group():
+    form = AddGroup()
+
+    if request.method == 'POST':
+        new_group = Ingredientgroup(group_name=form.group_name.data)
+        db.session.add(new_group)
+        db.session.commit()
+
+        return redirect(url_for('home'))
+
+    else:
+        return render_template('add_group.html', form=form)
 
 
 @app.route('/add_recipe')

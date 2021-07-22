4fdd7cbb7ebabe9d38c69cc226bb4dc9df57ecf6
Greg Nemeth <GNemeth@qa.com>
2021-07-22 11:43:22 +0000

    "GN:found a fixed bug in details route"

--- tipsy_tuesday/application/routes.py
+++ tipsy_tuesday/application/routes.py
@@ -187,7 +187,11 @@ def update_recipe(rec_id):
 def show_details(rec_id):
     dictionary = {}
     selected_recipe = Cocktailrecipes.query.get(rec_id)
-    ings_quants = db.session.query(Ingredient,Quantity).select_from(join(Ingredient, Junction)).join(Cocktailrecipes, Cocktailrecipes.rec_id == Junction.rec_id).join(Quantity, Quantity.quantity_id == Junction.quantity_id).filter(Junction.rec_id==Cocktailrecipes.rec_id).all()
+    ings_quants = db.session.query(Ingredient,Quantity)\
+        .select_from(join(Ingredient, Junction))\
+        .join(Cocktailrecipes, Cocktailrecipes.rec_id == Junction.rec_id)\
+        .join(Quantity, Quantity.quantity_id == Junction.quantity_id)\
+        .filter(Junction.rec_id==selected_recipe.rec_id).all()
 
     for i, q in ings_quants:
         dictionary[i] = q

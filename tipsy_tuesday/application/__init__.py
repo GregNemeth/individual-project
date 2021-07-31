af58b8fba3fa09607159389a41cde102aeb615ce
Greg Nemeth <GNemeth@qa.com>
2021-07-31 14:21:08 +0000

    GN:working without volume on mysql

--- tipsy_tuesday/application/__init__.py
+++ tipsy_tuesday/application/__init__.py
@@ -5,7 +5,7 @@ from os import getenv
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = str(os.getenv('DATABASE_URI'))  #change this to mysqlserver
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://root:{getenv('MYSQL_ROOT_PASSWORD')}@tipsy-db:3306/{getenv('MYSQL_DATABASE')}"  #change this to mysqlserver
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = str(os.urandom(16))
 
